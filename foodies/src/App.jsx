6ed807f26d85fa12d5bfa8b09a97f54736f30ac0
raufurkhan <raufurkhan82@gmail.com>
2025-12-27 02:27:07 +0530

    Explore Menu Implementation

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Menubar from "./components/Menubar/Menubar";
 import Contact from './pages/ContactUs/Contact'
-
-import ExploreFood from './pages/ExploreFood/ExploreFood'
-
 import Home from './pages/Home/Home'
 import { Routes, Route } from "react-router-dom";
+import ExploreFood from './pages/ExploreFood/ExploreFood';
 
 const App = () => {
   return (
     <div><Menubar/>
+   
     <Routes>
 <Route path='/' element={<Home />} />
 <Route path='/contact' element={<Contact />} />
