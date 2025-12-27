6956c9b36b32e5aff6c56add36c9d6c125e8464d
raufurkhan <raufurkhan82@gmail.com>
2025-12-27 23:01:51 +0530

    Food Display

---
+++ foodies/src/context/StoreContext.jsx
@@ -0,0 +1,36 @@
+import { createContext,useEffect,useState } from "react";
+import { fetchFoodList } from "../service/foodService";
+import axios from "axios";
+export const StoreContext = createContext(null);    
+
+export const StoreContextProvider = (props) => {
+
+const [foodList, setFoodList] = useState([]);
+
+
+
+
+const contextValue = {
+    foodList
+  };
+
+useEffect(()=>{
+async function loadData(){
+const data=await fetchFoodList();
+setFoodList(data)
+}
+loadData()
+},[])
+
+
+  
+
+
+
+
+      return (
+        <StoreContext.Provider value={contextValue}>
+          {props.children}
+        </StoreContext.Provider>
+      );
+}
