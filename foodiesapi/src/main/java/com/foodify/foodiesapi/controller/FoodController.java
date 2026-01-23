68386a44163ed29f40f3fff57a27b32bd51a7691
nakadeuttam <nakadeuttam66@gmail.com>
2026-01-23 15:17:56 +0530

    Aws Configuration and Food related endpoints

---
+++ foodiesapi/src/main/java/com/foodify/foodiesapi/controller/FoodController.java
@@ -0,0 +1,64 @@
+package com.foodify.foodiesapi.controller;
+
+import com.foodify.foodiesapi.io.FoodRequest;
+import com.foodify.foodiesapi.io.FoodResponse;
+import com.foodify.foodiesapi.service.FoodService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+//import tools.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/foods")
+@AllArgsConstructor
+@CrossOrigin("*")
+public class FoodController {
+
+    private final FoodService foodService;
+
+    @PostMapping
+    public FoodResponse addFood(@RequestPart("food") String foodString,
+                                @RequestPart("file")MultipartFile file) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        FoodRequest request = null;
+        try {
+            request = objectMapper.readValue(foodString, FoodRequest.class);
+        }catch(JsonProcessingException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid JSON format");
+        }
+        FoodResponse response = foodService.addFood(request, file);
+        return response;
+    }
+
+    @GetMapping
+    public List<FoodResponse> readFoods() {
+        return foodService.readFoods();
+    }
+
+
+    @GetMapping("/{id}")
+    public FoodResponse readFood(@PathVariable String id) {
+        return foodService.readFood(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFood(@PathVariable String id) {
+        foodService.deleteFood(id);
+    }
+
+
+
+}
