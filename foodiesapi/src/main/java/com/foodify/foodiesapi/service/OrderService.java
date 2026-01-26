e7dba791bdf2018c6825d71efcc20de57d5d9d30
nakadeuttam <nakadeuttam66@gmail.com>
2026-01-26 15:51:52 +0530

    Orders related endpoints

---
+++ foodiesapi/src/main/java/com/foodify/foodiesapi/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.foodify.foodiesapi.service;
+
+import com.foodify.foodiesapi.io.OrderRequest;
+import com.foodify.foodiesapi.io.OrderResponse;
+import com.razorpay.RazorpayException;
+
+
+import java.util.List;
+import java.util.Map;
+
+public interface OrderService {
+
+    OrderResponse createOrderWithPayment(OrderRequest request) throws RazorpayException;
+
+    void verifyPayment(Map<String, String> paymentData, String status);
+
+    List<OrderResponse> getUserOrders();
+
+    void removeOrder(String orderId);
+
+    List<OrderResponse> getOrdersOfAllUsers();
+
+    void updateOrderStatus(String orderId, String status);
+
+}
